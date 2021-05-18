eb31150cac4be8e887970775d92add1f86920330
DS_JH <j-hirano@delight-star.co.jp>
2021-05-18 22:41:11 +0900

    [upd] 初回プッシュ

---
+++ Instagram/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by DS_JH on 2021/05/11.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images" //Storage内の画像ファイルの保存場所定義
+    static let PostPath = "posts" // Firestore内の投稿データの保存場所を定義
+}
